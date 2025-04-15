e93e0449315cb413845f7bd4d25890f801828fcb
Pradhan Gurjar <gurjarpradhan043@gmail.com>
2025-04-15 22:27:35 +0530

    Code is Up to Date

--- palindrome.java
+++ palindrome.java
@@ -7,7 +7,7 @@ public class palindrome {
                 
             }
        
-    public static void main(String[] args) {~
+    public static void main(String[] args) {
       palindrome palidromeChecker = new palindrome();
       boolean check = palidromeChecker.isPalindrome(121);
       System.out.println("Is a palidrome ? "+ check); 

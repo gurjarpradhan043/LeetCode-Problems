1faff959747465692ac91012901395b3bd45b729
Bhagwan Shahay <124359288+gurjarpradhan043@users.noreply.github.com>
2026-07-12 00:11:41 +0530

    Time: 1 ms (16.15%) | Memory: 44.4 MB (73.73%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.java
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -2,16 +2,22 @@ class Solution {
     public int maximumWealth(int[][] accounts) {
         int richestCustomer = 0;
         int customerWealth = 0;
-        for(int i=0; i< accounts.length; i++) {
-            customerWealth = 0;
-            for(int j=0; j<accounts[i].length; j++) {
-                customerWealth += accounts[i][j];
-                if(customerWealth > richestCustomer ) {
-                    richestCustomer = customerWealth ;
-                }
 
+        for (int i = 0; i < accounts.length; i++) {
+
+            customerWealth = 0;
+
+            for (int j = 0; j < accounts[i].length; j++) {
+
+                customerWealth += accounts[i][j];
+
+                if (customerWealth > richestCustomer) {
+                    richestCustomer = customerWealth ;
+
+                }
             }
         }
+
         return richestCustomer;
     }
 }
